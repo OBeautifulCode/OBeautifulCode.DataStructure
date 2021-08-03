1dfa3bb321c904ef56855d4d15e1219633e94461
Suraj Gupta <SurajGupta@users.noreply.github.com>
2021-08-03 13:02:49 -0400

    remove ICell from IValidateableCell

--- OBeautifulCode.DataStructure/Interfaces/Classification/Cell/Convenience/IValidateableCell.cs
+++ OBeautifulCode.DataStructure/Interfaces/Classification/Cell/Convenience/IValidateableCell.cs
@@ -13,11 +13,11 @@ namespace OBeautifulCode.DataStructure
     /// This interface is a convenience for addressing the properties and methods required to
     /// validate <see cref="IOperationOutputCell{TValue}"/> and <see cref="IInputCell{TValue}"/>.
     /// If authoring your own <see cref="ICell"/> types, DO NOT implement this interface because
-    /// it may not be honored when performing validation. For example, if you author an
+    /// it will NOT be honored when performing validation. For example, if you author an
     /// <see cref="IConstOutputCell{TValue}"/> and mark it with this interface, the cell WILL NOT
     /// get validated.
     /// </remarks>
-    public interface IValidateableCell : ICell, IHaveCellValidationConditions, IValidateCellValue
+    public interface IValidateableCell : IHaveCellValidationConditions, IValidateCellValue
     {
     }
 }
