ad4b46f5c0820cf9e845af3877040ee284f876ef
Suraj Gupta <SurajGupta@users.noreply.github.com>
2022-01-22 13:05:32 -0500

    added DateTimFormat and added to ReportFormat for timestamp

---
+++ OBeautifulCode.DataStructure/Models/Classes/Format/DateTimeFormat.cs
@@ -0,0 +1,72 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DateTimeFormat.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.DataStructure
+{
+    using System;
+    using OBeautifulCode.Type;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// Specifies how to format a <see cref="DateTime"/>.
+    /// </summary>
+    public partial class DateTimeFormat : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DateTimeFormat"/> class.
+        /// </summary>
+        /// <param name="formatKind">OPTIONAL kind of formatting to apply.  DEFAULT is to let the consumer decide (e.g. use formatting kind from user's profile or always use a fixed formatting).</param>
+        /// <param name="cultureKind">OPTIONAL kind of culture to apply when formatting.  DEFAULT is let the consumer decide (e.g. use culture from user's profile or always use invariant culture).</param>
+        /// <param name="convertToTimeZone">OPTIONAL value that determines whether to convert a <see cref="DateTimeKind.Utc"/> <see cref="DateTime"/> to that time in some time zone before generating it's string representation.  DEFAULT is to let the consumer decide (e.g. never convert unless explicitly set to true).</param>
+        /// <param name="timeZoneToConvertInto">OPTIONAL time zone to convert a <see cref="DateTimeKind.Utc"/> <see cref="DateTime"/> into before generating it's string representation.  DEFAULT is to let the consumer decide (e.g. when <paramref name="convertToTimeZone"/> is true, use the user's local timezone).</param>
+        public DateTimeFormat(
+            DateTimeFormatKind? formatKind = null,
+            CultureKind? cultureKind = null,
+            bool? convertToTimeZone = null,
+            StandardTimeZone? timeZoneToConvertInto = null)
+        {
+            if ((formatKind != null) && (formatKind == DateTimeFormatKind.Unknown))
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"{nameof(formatKind)} is {nameof(DateTimeFormatKind.Unknown)}."));
+            }
+
+            if ((cultureKind != null) && (cultureKind == OBeautifulCode.Type.CultureKind.Unknown))
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"{nameof(cultureKind)} is {nameof(OBeautifulCode.Type.CultureKind.Unknown)}."));
+            }
+
+            if ((convertToTimeZone == false) && (timeZoneToConvertInto != null))
+            {
+                throw new ArgumentException(Invariant($"{nameof(convertToTimeZone)} is false, but {nameof(timeZoneToConvertInto)} is not null."));
+            }
+
+            this.FormatKind = formatKind;
+            this.CultureKind = cultureKind;
+            this.ConvertToTimeZone = convertToTimeZone;
+            this.TimeZoneToConvertInto = timeZoneToConvertInto;
+        }
+
+        /// <summary>
+        /// Gets the kind of formatting to apply.
+        /// </summary>
+        public DateTimeFormatKind? FormatKind { get; private set; }
+
+        /// <summary>
+        /// Gets the kind of culture to apply when formatting.
+        /// </summary>
+        public CultureKind? CultureKind { get; private set; }
+
+        /// <summary>
+        /// Gets a value that determines whether to convert a <see cref="DateTimeKind.Utc"/> <see cref="DateTime"/> to that time in some time zone before generating it's string representation.
+        /// </summary>
+        public bool? ConvertToTimeZone { get; private set; }
+
+        /// <summary>
+        /// Gets the time zone to convert a <see cref="DateTimeKind.Utc"/> <see cref="DateTime"/> into before generating it's string representation.
+        /// </summary>
+        public StandardTimeZone? TimeZoneToConvertInto { get; private set; }
+    }
+}
--- OBeautifulCode.DataStructure/Models/Classes/Format/Report/ReportFormat.cs
+++ OBeautifulCode.DataStructure/Models/Classes/Format/Report/ReportFormat.cs
@@ -6,12 +6,41 @@
 
 namespace OBeautifulCode.DataStructure
 {
+    using System;
     using OBeautifulCode.Type;
+    using static System.FormattableString;
 
     /// <summary>
     /// The format to apply to a <see cref="Report"/>.
     /// </summary>
     public partial class ReportFormat : IModelViaCodeGen
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReportFormat"/> class.
+        /// </summary>
+        /// <param name="displayTimestamp">OPTIONAL value that specifies whether to display the report's timestamp.  DEFAULT is to let the consumer decide (e.g. always display the timestamp when it's not null).</param>
+        /// <param name="timestampFormat">OPTIONAL format to apply to the timestamp when displaying it.  DEFAULT is to let the consumer decide (e.g. when displaying the timestamp, use a pre-specified/standard format).</param>
+        public ReportFormat(
+            bool? displayTimestamp = null,
+            DateTimeFormat timestampFormat = null)
+        {
+            if ((displayTimestamp == false) && (timestampFormat != null))
+            {
+                throw new ArgumentException(Invariant($"{nameof(displayTimestamp)} is false, but {nameof(timestampFormat)} is not null."));
+            }
+
+            this.DisplayTimestamp = displayTimestamp;
+            this.TimestampFormat = timestampFormat;
+        }
+
+        /// <summary>
+        /// Gets a value that specifies whether to display the report's timestamp.
+        /// </summary>
+        public bool? DisplayTimestamp { get; private set; }
+
+        /// <summary>
+        /// Gets the format to apply to the timestamp when displaying it.
+        /// </summary>
+        public DateTimeFormat TimestampFormat { get; private set; }
     }
 }
