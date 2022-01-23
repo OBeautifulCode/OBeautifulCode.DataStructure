d43ae4ddc11885ccb0e040976d660a269070f7db
Suraj Gupta <SurajGupta@users.noreply.github.com>
2022-01-23 18:16:04 -0500

    added scaffolding for projection of Report to Workbook

--- OBeautifulCode.DataStructure.Excel/ReportProjector.cs
+++ OBeautifulCode.DataStructure.Excel/ReportProjector.cs
@@ -6,10 +6,27 @@
 
 namespace OBeautifulCode.DataStructure.Excel
 {
+    using System;
+    using Aspose.Cells;
+
     /// <summary>
     /// Projects a <see cref="Report"/> into an Excel workbook.
     /// </summary>
     public static class ReportProjector
     {
+        /// <summary>
+        /// Projects a <see cref="Report"/> into a <see cref="Workbook"/>.
+        /// </summary>
+        /// <param name="report">The report.</param>
+        /// <param name="context">The context to use.</param>
+        /// <returns>
+        /// The <see cref="Workbook"/>.
+        /// </returns>
+        public static Workbook ToExcelWorkbook(
+            this Report report,
+            ReportToWorkbookProjectionContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ OBeautifulCode.DataStructure.Excel/ReportToWorkbookProjectionContext.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ReportToWorkbookProjectionContext.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.DataStructure.Excel
+{
+    using Aspose.Cells;
+
+    /// <summary>
+    /// Context object for a <see cref="Report"/> to <see cref="Workbook"/> projection.
+    /// </summary>
+    public class ReportToWorkbookProjectionContext
+    {
+    }
+}
