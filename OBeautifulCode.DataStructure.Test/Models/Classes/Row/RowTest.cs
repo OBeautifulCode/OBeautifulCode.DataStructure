2e70722028faee1ec99a650ac4f9f681b43de365
Suraj Gupta <SurajGupta@users.noreply.github.com>
2024-10-03 10:49:51 -0400

    fix failing unit tests

--- OBeautifulCode.DataStructure.Test/Models/Classes/Row/RowTest.cs
+++ OBeautifulCode.DataStructure.Test/Models/Classes/Row/RowTest.cs
@@ -31,6 +31,70 @@ namespace OBeautifulCode.DataStructure.Test
         static RowTest()
         {
             ConstructorArgumentValidationTestScenarios
+                .RemoveAllScenarios()
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
+                        Name = "constructor should throw ArgumentNullException when parameter 'cells' is null scenario",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                null,
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                referenceObject.ChildRows,
+                                referenceObject.ExpandedSummaryRows,
+                                referenceObject.CollapsedSummaryRows);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentNullException),
+                        ExpectedExceptionMessageContains = new[] { "cells", },
+                    })
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
+                        Name = "constructor should throw ArgumentException when parameter 'cells' is an empty enumerable scenario",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                new List<ICell>(),
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                referenceObject.ChildRows,
+                                referenceObject.ExpandedSummaryRows,
+                                referenceObject.CollapsedSummaryRows);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentException),
+                        ExpectedExceptionMessageContains = new[] { "cells", "is an empty enumerable", },
+                    })
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
+                        Name = "constructor should throw ArgumentException when parameter 'cells' contains a null element scenario",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                new ICell[0].Concat(referenceObject.Cells).Concat(new ICell[] { null }).Concat(referenceObject.Cells).ToList(),
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                referenceObject.ChildRows,
+                                referenceObject.ExpandedSummaryRows,
+                                referenceObject.CollapsedSummaryRows);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentException),
+                        ExpectedExceptionMessageContains = new[] { "cells", "contains at least one null element", },
+                    })
                 .AddScenario(() =>
                     new ConstructorArgumentValidationTestScenario<Row>
                     {
@@ -51,6 +115,48 @@ namespace OBeautifulCode.DataStructure.Test
                         },
                         ExpectedExceptionType = typeof(ArgumentException),
                         ExpectedExceptionMessageContains = new[] { "id", "white space", },
+                    })
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
+                        Name = "constructor should throw ArgumentException when parameter 'childRows' contains a null element scenario",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                referenceObject.Cells,
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                new Row[0].Concat(referenceObject.ChildRows).Concat(new Row[] { null }).Concat(referenceObject.ChildRows).ToList(),
+                                referenceObject.ExpandedSummaryRows,
+                                referenceObject.CollapsedSummaryRows);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentException),
+                        ExpectedExceptionMessageContains = new[] { "childRows", "contains at least one null element", },
+                    })
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
+                        Name = "constructor should throw ArgumentException when parameter 'expandedSummaryRows' contains a null element.",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                referenceObject.Cells,
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                referenceObject.ChildRows,
+                                new[] { A.Dummy<FlatRow>(), null, A.Dummy<FlatRow>() },
+                                null);
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentException),
+                        ExpectedExceptionMessageContains = new[] { "expandedSummaryRows contains at least one null element", },
                     })
                 .AddScenario(() =>
                     new ConstructorArgumentValidationTestScenario<Row>
@@ -97,6 +203,27 @@ namespace OBeautifulCode.DataStructure.Test
                 .AddScenario(() =>
                     new ConstructorArgumentValidationTestScenario<Row>
                     {
+                        Name = "constructor should throw ArgumentException when parameter 'collapsedSummaryRows' contains a null element.",
+                        ConstructionFunc = () =>
+                        {
+                            var referenceObject = A.Dummy<Row>();
+
+                            var result = new Row(
+                                referenceObject.Cells,
+                                referenceObject.Id,
+                                referenceObject.Format,
+                                null,
+                                null,
+                                new[] { A.Dummy<FlatRow>(), null, A.Dummy<FlatRow>() });
+
+                            return result;
+                        },
+                        ExpectedExceptionType = typeof(ArgumentException),
+                        ExpectedExceptionMessageContains = new[] { "collapsedSummaryRows contains at least one null element", },
+                    })
+                .AddScenario(() =>
+                    new ConstructorArgumentValidationTestScenario<Row>
+                    {
                         Name = "constructor should throw ArgumentException when parameter 'collapsedSummaryRows' contains rows, but childRows is null.",
                         ConstructionFunc = () =>
                         {
@@ -135,129 +262,6 @@ namespace OBeautifulCode.DataStructure.Test
                         },
                         ExpectedExceptionType = typeof(ArgumentException),
                         ExpectedExceptionMessageContains = new[] { "collapsedSummaryRows is specified when there are no rows in childRows", },
-                    });
-
-            DeepCloneWithTestScenarios
-                .RemoveAllScenarios()
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithId should deep clone object and replace Id with the provided id",
-                        WithPropertyName = "Id",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>();
-
-                            var referenceObject = A.Dummy<Row>().ThatIs(_ => !systemUnderTest.Id.IsEqualTo(_.Id));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.Id,
-                            };
-
-                            return result;
-                        },
-                    })
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithCells should deep clone object and replace Cells with the provided cells",
-                        WithPropertyName = "Cells",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>();
-
-                            var referenceObject = A.Dummy<Row>().ThatIs(_ => !systemUnderTest.Cells.IsEqualTo(_.Cells));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.Cells,
-                            };
-
-                            return result;
-                        },
-                    })
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithFormat should deep clone object and replace Format with the provided format",
-                        WithPropertyName = "Format",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>();
-
-                            var referenceObject = A.Dummy<Row>().ThatIs(_ => !systemUnderTest.Format.IsEqualTo(_.Format));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.Format,
-                            };
-
-                            return result;
-                        },
-                    })
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithChildRows should deep clone object and replace ChildRows with the provided childRows",
-                        WithPropertyName = "ChildRows",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>().Whose(_ => _.ChildRows.Any());
-
-                            var referenceObject = A.Dummy<Row>().Whose(_ => _.ChildRows.Any() && (!_.ChildRows.IsEqualTo(systemUnderTest.ChildRows)));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.ChildRows,
-                            };
-
-                            return result;
-                        },
-                    })
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithExpandedSummaryRows should deep clone object and replace ExpandedSummaryRows with the provided expandedSummaryRows",
-                        WithPropertyName = "ExpandedSummaryRows",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>().Whose(_ => _.ChildRows.Any());
-
-                            var referenceObject = A.Dummy<Row>().Whose(_ => _.ChildRows.Any() && (!_.ExpandedSummaryRows.IsEqualTo(systemUnderTest.ExpandedSummaryRows)));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.ExpandedSummaryRows,
-                            };
-
-                            return result;
-                        },
-                    })
-                .AddScenario(() =>
-                    new DeepCloneWithTestScenario<Row>
-                    {
-                        Name = "DeepCloneWithCollapsedSummaryRows should deep clone object and replace CollapsedSummaryRows with the provided collapsedSummaryRows",
-                        WithPropertyName = "CollapsedSummaryRows",
-                        SystemUnderTestDeepCloneWithValueFunc = () =>
-                        {
-                            var systemUnderTest = A.Dummy<Row>().Whose(_ => _.ChildRows.Any());
-
-                            var referenceObject = A.Dummy<Row>().Whose(_ => _.ChildRows.Any() && (!_.CollapsedSummaryRows.IsEqualTo(systemUnderTest.CollapsedSummaryRows)));
-
-                            var result = new SystemUnderTestDeepCloneWithValue<Row>
-                            {
-                                SystemUnderTest = systemUnderTest,
-                                DeepCloneWithValue = referenceObject.CollapsedSummaryRows,
-                            };
-
-                            return result;
-                        },
                     });
 
             EquatableTestScenarios
