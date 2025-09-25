9b1ab0952b202b0d3fd5c00b1d30baaf91c104c4
Suraj Gupta <SurajGupta@users.noreply.github.com>
2025-09-25 12:58:56 -0400

    added MultiActionButton and MultiActionButtonItem

---
+++ OBeautifulCode.DataStructure/Models/Classes/CellValue/Controls/MultiActionButton.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MultiActionButton.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.DataStructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using OBeautifulCode.CodeAnalysis.Recipes;
+    using OBeautifulCode.Type;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// A button that supports multiple actions.
+    /// </summary>
+    /// <remarks>
+    /// A nice user experience for this is a button that, when pressed, reveals a set of options
+    /// in a dropdown selector, and when the desired option is selected, the action is executed.
+    /// </remarks>
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class MultiActionButton : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiActionButton"/> class.
+        /// </summary>
+        /// <param name="buttonText">The text to display on the button.</param>
+        /// <param name="actionItems">The action items in the order they should be displayed.</param>
+        [SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings", MessageId = "0#", Justification = ObcSuppressBecause.CA1054_UriParametersShouldNotBeStrings_PreferToRepresentUrlAsString)]
+        public MultiActionButton(
+            string buttonText,
+            IReadOnlyList<MultiActionButtonItem> actionItems)
+        {
+            if (buttonText == null)
+            {
+                throw new ArgumentNullException(nameof(buttonText));
+            }
+
+            if (string.IsNullOrWhiteSpace(buttonText))
+            {
+                throw new ArgumentException(Invariant($"{nameof(buttonText)} is white space."), nameof(buttonText));
+            }
+
+            if (actionItems == null)
+            {
+                throw new ArgumentNullException(nameof(actionItems));
+            }
+
+            if (!actionItems.Any())
+            {
+                throw new ArgumentException(Invariant($"{nameof(actionItems)} is an empty enumerable."));
+            }
+
+            if (actionItems.Any(_ => _ == null))
+            {
+                throw new ArgumentException(Invariant($"{nameof(actionItems)} contains at least one null element."));
+            }
+
+            this.ButtonText = buttonText;
+            this.ActionItems = actionItems;
+        }
+
+        /// <summary>
+        /// Gets the text to display on the button.
+        /// </summary>
+        public string ButtonText { get; private set; }
+
+        /// <summary>
+        /// Gets the action items in the order they should be displayed.
+        /// </summary>
+        public IReadOnlyList<MultiActionButtonItem> ActionItems { get; private set; }
+    }
+}
---
+++ OBeautifulCode.DataStructure/Models/Classes/CellValue/Controls/MultiActionButtonItem.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MultiActionButtonItem.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.DataStructure
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using OBeautifulCode.CodeAnalysis.Recipes;
+    using OBeautifulCode.Type;
+    using static System.FormattableString;
+
+    /// <summary>
+    /// An action item in a <see cref="MultiActionButton"/>.
+    /// </summary>
+    public partial class MultiActionButtonItem : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiActionButtonItem"/> class.
+        /// </summary>
+        /// <param name="text">The text to link.</param>
+        /// <param name="link">The link.</param>
+        /// <param name="availability">OPTIONAL availability of the action item.  DEFAULT is an enabled action.</param>
+        /// <param name="media">OPTIONAL media to display alongside the linked text.  DEFAULT is no media.</param>
+        /// <param name="hoverOverText">OPTIONAL hover-over text.  DEFAULT is no hover-over text.</param>
+        [SuppressMessage("Microsoft.Design", "CA1054:UriParametersShouldNotBeStrings", MessageId = "0#", Justification = ObcSuppressBecause.CA1054_UriParametersShouldNotBeStrings_PreferToRepresentUrlAsString)]
+        public MultiActionButtonItem(
+            string text,
+            ILink link,
+            Availability availability = Availability.Enabled,
+            IMedia media = null,
+            string hoverOverText = null)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException(Invariant($"{nameof(text)} is white space."), nameof(text));
+            }
+
+            if (link == null)
+            {
+                throw new ArgumentNullException(nameof(link));
+            }
+
+            if (availability == Availability.Unknown)
+            {
+                throw new ArgumentOutOfRangeException(Invariant($"{nameof(availability)} is {nameof(DataStructure.Availability)}.{nameof(Availability.Unknown)}."), nameof(availability));
+            }
+
+            this.Text = text;
+            this.Link = link;
+            this.Availability = availability;
+            this.Media = media;
+            this.HoverOverText = hoverOverText;
+        }
+
+        /// <summary>
+        /// Gets the text to link.
+        /// </summary>
+        public string Text { get; private set; }
+
+        /// <summary>
+        /// Gets the link.
+        /// </summary>
+        public ILink Link { get; private set; }
+
+        /// <summary>
+        /// Gets the availability of the action item.
+        /// </summary>
+        public Availability Availability { get; private set; }
+
+        /// <summary>
+        /// Gets the media to display alongside the linked text.
+        /// </summary>
+        public IMedia Media { get; private set; }
+
+        /// <summary>
+        /// Gets the hover-over text.
+        /// </summary>
+        public string HoverOverText { get; private set; }
+    }
+}
